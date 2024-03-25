d71ced8dcc27ce055f4fe9888ded9d6de7762730
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-24 19:42:12 -0500

    updates to files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,15 @@
 -- schema.sql
 
+INSERT INTO department (name) VALUES
+    ('Human Resources'),
+    ('Accounting'),
+    ('Marketing');
+
+INSERT INTO role (title, salary, department_id) VALUES
+    ('Human Resources Manager', 80000, 1),
+    ('Accountant', 60000, 2),
+    ('Marketing Manager', 70000, 3);
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
     ('Diana', 'Mendez', 1, NULL),
     ('Michael', 'Rodriguez', 2, 1),
