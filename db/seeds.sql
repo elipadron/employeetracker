a01f128d27ae0432a32aafc1f411c5c05b12e35e
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-24 19:35:11 -0500

    update to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
 -- schema.sql
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ('Diana', 'Mendez', 1, NULL),
+    ('Michael', 'Rodriguez', 2, 1),
+    ('Jesus', 'Smith', 3, 1);
 
